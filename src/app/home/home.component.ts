17da3f78ffb996ed2a02351317b63b67047d5b3f
Lalith <lalith0208e@gmail.com>
2025-06-11 15:17:02 +0530

    heade and home page change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+  export class HomeComponent {
+    // Helper to get the current year for the footer
+    getCurrentYear(): number {
+      return new Date().getFullYear();
+    }
 }
